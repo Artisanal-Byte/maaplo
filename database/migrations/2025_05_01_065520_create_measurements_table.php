a471c0c75db9351b98b55b4ba2e11aee72cfe039
Goswami Sahil <goswamisahil1911@gmail.com>
2025-05-09 12:17:52 +0530

    Remove 'templates_measurements_id' column in measurements migration

--- database/migrations/2025_05_01_065520_create_measurements_table.php
+++ database/migrations/2025_05_01_065520_create_measurements_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('measurements', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('templates_measurements_id');
+            // $table->unsignedBigInteger('templates_measurements_id');
             $table->bigInteger('slug');
             $table->text('text');
             $table->timestamps();
