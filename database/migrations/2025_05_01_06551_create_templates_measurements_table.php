21264b370bbd404c03feea0380ed6ff26eea7c0a
Goswami Sahil <goswamisahil1911@gmail.com>
2025-05-09 12:22:04 +0530

    add template_id and Fix foreign key definition in templates_measurements migration

--- database/migrations/2025_05_01_06551_create_templates_measurements_table.php
+++ database/migrations/2025_05_01_06551_create_templates_measurements_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('templates_measurements', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('measurements_id');
+            $table->unsignedBigInteger('template_id');
             $table->timestamps();
 
-           
+            $table->foreign('template_id')->references('id')->on('templates')->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreign('measurements_id')->references('id')->on('templates_measurements')->cascadeOnUpdate()->cascadeOnDelete();
         });
     }
