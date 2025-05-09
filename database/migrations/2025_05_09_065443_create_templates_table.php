e3296deafb7fe1d1cde9c352ce9b4eef8c767593
Goswami Sahil <goswamisahil1911@gmail.com>
2025-05-09 12:27:22 +0530

    rm database/migrations/0001_01_01_000003_create_item_templates_table.php

--- database/migrations/2025_05_09_065443_create_templates_table.php
+++ database/migrations/2025_05_09_065443_create_templates_table.php
@@ -34,6 +34,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('item_templates');
+        Schema::dropIfExists('templates');
     }
 };
