bb46508894c460ceb4d0f614f417da8cfe018c75
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-05-13 10:26:00 +0530

    Remove required attribute from Trial and Delivery Date inputs in TrialAndDeliveryDate.vue

--- resources/js/components/TrialAndDeliveryDate.vue
+++ resources/js/components/TrialAndDeliveryDate.vue
@@ -18,7 +18,7 @@ watch(()=>deliveryDate.value,(newVal)=>{
     <div class="flex flex-col lg:flex-row justify-between">
         <div class="lg:gap-2 gap-2 flex flex-col lg:w-96">
             <div class="flex">
-                <Input type="date" id="trail-date" v-model="trialDate" :required="true" label="Trail Date:" width="lg"  name="trail-date"
+                <Input type="date" id="trail-date" v-model="trialDate" label="Trail Date :" width="lg" name="trail-date"
                     color="grayBorder" margin="md" />
                 <Icon icon="mingcute:delete-fill" width="24" height="24" class="mt-10 ml-2" />
             </div>
@@ -27,7 +27,7 @@ watch(()=>deliveryDate.value,(newVal)=>{
         <!-- Delivery Date -->
         <div class="lg:gap-2 gap-2 flex flex-col lg:w-96">
             <div>
-                <Input type="date" id="delivery-date"   v-model="deliveryDate"  label="Delivery Date :" width="lg" :required="true" name="delivery-date"
+                <Input type="date" id="delivery-date"   v-model="deliveryDate"  label="Delivery Date :" width="lg" name="delivery-date"
                     color="grayBorder" margin="md" />
             </div>
         </div>
