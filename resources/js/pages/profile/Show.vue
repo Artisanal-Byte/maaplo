b06fb4984c84d2d653272ad0b6cd235e3507f3bc
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-05-13 14:34:52 +0530

    Enhance profile page layout and input fields; add reactive customer data handling

--- resources/js/pages/profile/Show.vue
+++ resources/js/pages/profile/Show.vue
@@ -1,12 +1,52 @@
 <script setup>
 const props = defineProps(['user'])
+import Button from '@/components/Button.vue';
+import Input from '@/components/InputWithLabel.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
+import { reactive } from 'vue';
+const customer = reactive({
+  name: 'Jermaine Reeves',
+  email: 'vanobedoku@mailinator.com'
+});
 </script>
 <template>
-    <AppLayout>
-      <pre>
+  <AppLayout>
+    <!-- <pre>
         {{ user }}
-      </pre>
-    </AppLayout>
+      </pre> -->
+    <div class="px-4 py-8 max-w-6xl mx-auto">
+      <h1 class="text-[24px] leading-[16px] font-bold tracking-[0] text-gray-800 font-[Convergence]">
+        Profile
+      </h1>
+      <div class="flex flex-col lg:mt-5 gap-3 rounded-lg lg:border lg:border-primary p-0 lg:p-4">
+        <h1 class="text-xl font-bold lg:mt-0 mt-6">Enter Profile Details</h1>
+        <div>
+          <!-- <span class="text-red-500">*</span> -->
+          <Input type="text" label="Name" color="grayBorder" :required="true" placeholder="Enter Customer Name"
+            v-model="customer.name" />
+        </div>
+        <div>
+          <Input type="email" label="Email" color="grayBorder" :required="true" v-model="customer.email" />
+        </div>
+        <div>
+          <Input type="text" label="Phone" color="grayBorder" :required="true" />
+        </div>
+        <div>
+          <Input type="text" label="Organization Name" color="grayBorder" :required="true" />
+        </div>
+        <div>
+          <Input type="url" label="Organization Logo" color="grayBorder" :required="true" />
+        </div>
+        <div>
+          <Input type="text" label="Subscription Plan" color="grayBorder" :required="true" />
+        </div>
+        <div>
+          <Input type="date" label="Validity" color="grayBorder" :required="true" />
+        </div>
+        <Button :color="'primary'" :padding="'md'" :rounded="'full'" :textSize="'sm'">
+          Submit
+        </Button>
+      </div>
+    </div>
+  </AppLayout>
 </template>
-
