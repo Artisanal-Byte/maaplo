af052b139290b79ab1e992c9eab9a3baaee3f789
Goswami Sahil <goswamisahil1911@gmail.com>
2025-05-14 16:03:46 +0530

    Fix error message display for body part validation in Create.vue

--- resources/js/pages/items/Create.vue
+++ resources/js/pages/items/Create.vue
@@ -139,7 +139,7 @@ const submitForm = () => {
                         </label>
                     </div>
                 </div>
-                <div v-if="errors.gender" class="text-red-600 text-sm">{{ errors.gender }}</div>
+                <div v-if="errors.body_part" class="text-red-600 text-sm">{{ errors.body_part }}</div>
                 <div>
                     <Input v-model="form.svg_logo" label="SVG Logo" placeholder="Paste SVG path here" margin="md"
                         width="full" fonttype="normal" textSize="base" rounded="md" :error="errors.svg_logo" />
