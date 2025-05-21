90ce98e588b283d6bd9ce61003b80e498102f1b5
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-05-21 12:57:16 +0530

    Add error handling to profile form inputs and update props for validation

--- resources/js/pages/profile/Show.vue
+++ resources/js/pages/profile/Show.vue
@@ -4,7 +4,7 @@ import Input from '@/components/InputWithLabel.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { useForm } from '@inertiajs/vue3';
 
-const props = defineProps(['user'])
+const props = defineProps(['user','errors'])
 const form = useForm({
   name: props.user.name || '',
   email: props.user.email || '',
@@ -43,28 +43,28 @@ function submit() {
         <div>
           <!-- <span class="text-red-500">*</span> -->
           <Input type="text" label="Name" color="grayBorder" :required="true" placeholder="Enter Customer Name"
-            v-model="form.name" />
+            v-model="form.name" :error="errors.name" />
         </div>
         <div>
-          <Input type="email" label="Email" color="grayBorder" :required="true" v-model="form.email" />
+          <Input type="email" label="Email" color="grayBorder" :required="true" v-model="form.email" :error="errors.email" />
         </div>
         <div>
           <Input type="text" label="Phone" color="grayBorder" :required="true" v-model="form.phone" />
         </div>
         <div>
-          <Input type="text" label="Organization Name" color="grayBorder" :required="true" v-model="form.organization_name" />
+          <Input type="text" label="Organization Name" color="grayBorder" v-model="form.organization_name" />
         </div>
         <div>
-          <Input type="file" label="Organization Logo" color="grayBorder" :required="true" v-model="form.organization_logo" />
+          <Input type="file" label="Organization Logo" color="grayBorder"  v-model="form.organization_logo" />
         </div>
         <div>
-          <Input type="text" label="Subscription Plan" color="grayBorder" :required="true" v-model="form.subscription_plan" />
+          <Input type="text" label="Subscription Plan" color="grayBorder" :required="true" v-model="form.subscription_plan" :error="errors.subscription_plan" />
           <!-- <Button :color="'primary'" :padding="'md'" :rounded="'full'" :textSize="'sm'" @click="submit">
             Upgrade Plan
           </Button> -->
         </div>
         <div>
-          <Input type="date" label="Validity" color="grayBorder" :required="true" v-model="form.validity" />
+          <Input type="date" label="Validity" color="grayBorder"  v-model="form.validity" />
         </div>
         <Button :color="'primary'" :padding="'md'" :rounded="'full'" :textSize="'sm'" @click="submit">
           Submit
