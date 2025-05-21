e14cee5746ec4beefed6e2e40a19a13ffc767772
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-05-21 15:25:31 +0530

    AppSidebar and AppSidebarHeader components for improved functionality and structure

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -91,7 +91,7 @@ const visible = ref(false);
                     </li>
                     <li>
                         <div @click="toggleDropdown()" class="flex rounded hover:bg-gray-200 text-black">
-                            <div @click="visible = false" class="flex items-center p-2 rounded ">
+                            <div class="flex items-center p-2 rounded">
                             <Icon icon="lets-icons:order-fill" width="24" height="24" />
                             <span
                                 class="ml-2 font-inter font-medium text-[16px] leading-[16px] tracking-[0]">Order</span>
