46a4aab2d7408d35f8929b21a682528b1f8f9e5f
Goswami Sahil <goswamisahil1911@gmail.com>
2025-05-21 18:47:20 +0530

    admin Index page layout and add Create Admin button

--- resources/js/pages/admin/Index.vue
+++ resources/js/pages/admin/Index.vue
@@ -2,6 +2,7 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { Icon } from '@iconify/vue';
 import { defineProps } from 'vue';
+import { Link } from '@inertiajs/vue3';
 
 const props = defineProps({
     users: Array
@@ -13,12 +14,30 @@ console.log(props.users);
 <template>
     <AppLayout>
         <div class="lg:mx-auto max-w-7xl py-8 px-4">
-            <div>
-                <h1 class="text-[24px] mt-3 leading-[16px] font-bold tracking-[0] text-gray-800 font-[Convergence]">
-                    Admin
-                </h1>
+            <!-- Header -->
+            <div class="flex justify-between items-center mb-6">
+                <div>
+                    <h1 class="text-[24px] mt-3 leading-[16px] font-bold tracking-[0] text-gray-800 font-[Convergence]">
+                        Admin
+                    </h1>
+                    <p class="text-sm text-gray-700 text-right mt-2">
+                        Total Admins: {{ users.length }}
+                    </p>
+                </div>
+
+                <!-- Create Admin Button -->
+                <div class="relative group mt-1">
+                    <Link :href="route('user.create')" class="cursor-pointer">
+                        <Icon icon="material-symbols:add-rounded" width="30" height="30" />
+                    </Link>
+                    <div
+                        class="absolute top-full mt-2 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition bg-gray-800 text-white text-xs rounded py-1 px-2 pointer-events-none z-10">
+                        Create Admin
+                    </div>
+                </div>
             </div>
 
+            <!-- Admin Cards -->
             <div class="grid lg:grid-cols-2 grid-cols-1 gap-10">
                 <div
                     v-for="user in users"
@@ -56,6 +75,16 @@ console.log(props.users);
                             </div>
                         </div>
 
+                        <!-- Delete -->
+                        <!-- <div class="relative group">
+                            <button class="flex items-center text-red-500 hover:text-red-600 transition">
+                                <Icon icon="ic:baseline-delete" width="18" height="18" class="text-[#E73939]" />
+                            </button>
+                            <div
+                                class="absolute top-full mt-2 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition bg-gray-800 text-white text-xs rounded py-1 px-2 pointer-events-none z-10">
+                                Delete
+                            </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
