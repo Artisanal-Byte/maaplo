bbb204cfcbd948f9c8cdccb4a9df6af203a39937
Goswami Sahil <goswamisahil1911@gmail.com>
2025-05-22 18:05:35 +0530

    user management interface: update imageProccess method to include type parameter and conditional folder path handling

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -18,16 +18,24 @@ class ImageHelper
      * @param string $label
      * @return string
      */
-    public static function imageProccess(UploadedFile $image, $customerId, $username, $userId, $customerName, $label)
+    public static function imageProccess(UploadedFile $image, $customerId, $username, $userId, $customerName, $label = 'organization_logo', $type = 'user')
     {
         try {
             $timestamp = time();
             $fileName = "{$label}_{$timestamp}.webp";
-            $folderPath = "customers/{$username}_{$userId}/{$customerName}_{$customerId}/images";
+
+            // Conditional folder path
+            if ($type === 'customer') {
+                $folderPath = "customers/{$username}_{$userId}/images";
+            } else {
+                $folderPath = "users_organization_logo/{$username}_{$userId}";
+            }
+
+            // $folderPath = "  customers/{$username}_{$userId}/{$customerName}_{$customerId}/images";
             $fullPath = "{$folderPath}/{$fileName}";
+            $directory = storage_path("app/public/{$folderPath}/");
 
             // Define storage path
-            $directory = storage_path('app/public/' . $folderPath . '/');
             if (!file_exists($directory)) {
                 mkdir($directory, 0755, true);
             }
@@ -37,7 +45,7 @@ class ImageHelper
             $manager = new ImageManager(config('image.driver'));
             $image = $manager->read($image);
 
-           $image = $image->scaleDown(width: 2000, height: 2000);
+            $image = $image->scaleDown(width: 2000, height: 2000);
             // Read and encode image
             $image = $manager->read($image);
             $encoded = $image->toWebp(60);
