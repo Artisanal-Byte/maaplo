6d954b2c7fa844882cb6eb0bee4319edfaefb9dd
Goswami Sahil <goswamisahil1911@gmail.com>
2025-05-23 17:51:38 +0530

    add thumbnail_logo field to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration {
             $table->string('organization_name')->nullable();
             $table->string('address')->nullable();
             $table->string('organization_logo')->nullable();
+            $table->string('thumbnail_logo')->nullable();
             $table->enum('subscription_plan', ['free'])->default('free');
             $table->boolean('status')->default(true);
             $table->date('validity')->nullable();
