1dacd1d906fd0460ee182541fd55fe9fd8a09794
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-05-26 10:55:57 +0530

    Add Logo component and update AppSidebar to use it

---
+++ resources/js/components/Logo.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+<template>
+    <div class="flex justify-center items-center h-full">
+        <img src="/images/maaplologo.png" alt="Logo" class="h-10 w-10" />
+    </div>
+</template>
