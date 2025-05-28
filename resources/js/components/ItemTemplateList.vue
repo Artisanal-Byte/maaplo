5eb1914b9025332991a12f74ace9cd15fc44c83d
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-05-28 12:02:13 +0530

    Update ItemTemplateList styles for consistency and improve edit icon color

--- resources/js/components/ItemTemplateList.vue
+++ resources/js/components/ItemTemplateList.vue
@@ -12,14 +12,14 @@ const emit = defineEmits(['delete']);
 </script>
 
 <template>
-  <div v-if="view === 'card'" class="bg-white shadow rounded p-4">
+  <div v-if="view === 'card'" class="bg-[#DEEFF4] shadow rounded p-4">
     <h2 class="text-xl font-semibold">{{ item.name }}</h2>
     <p class="text-gray-700 mt-2">Gender: {{ item.gender }}</p>
     <p class="text-gray-700 mt-1">Body Part: {{ item.body_part }}</p>
 
     <div v-if="item.user_id === authUser.id" class="flex justify-end gap-3 mt-3">
       <Link :href="route('items.edit', item.id)">
-        <Icon icon="ri:edit-fill" class="text-blue-600" width="20" height="20" />
+        <Icon icon="ri:edit-fill" class="text-primary" width="20" height="20" />
       </Link>
       <button @click="$emit('delete', item)">
         <Icon icon="ic:baseline-delete" class="text-red-600" width="20" height="20" />
