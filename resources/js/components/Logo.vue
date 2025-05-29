5a42840217326096d2a33eacf966a1f691957a09
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-05-29 12:48:17 +0530

    Logo component to include Link import and wrap image in Link for navigation to dashboard.

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { usePage } from '@inertiajs/vue3'
+import { Link, usePage } from '@inertiajs/vue3'
 
 const page = usePage()
 
@@ -14,9 +14,11 @@ const fallbackLogo = '/images/maaplologo.png'
 </script>
 
 <template>
+  <Link :href="route('dashboard')">
   <img
     :src="thumbnailLogo || fallbackLogo"
     alt="Organization Logo"
     class="h-10 w-10"
   />
+  </Link>
 </template>
