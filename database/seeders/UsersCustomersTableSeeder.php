c50ad181404f28c70a201c83cacbd1e5488f11c6
Goswami Sahil <goswamisahil1911@gmail.com>
2025-07-18 10:14:24 +0530

    solve customer seeder error

--- database/seeders/UsersCustomersTableSeeder.php
+++ database/seeders/UsersCustomersTableSeeder.php
@@ -14,32 +14,29 @@ class UsersCustomersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('users_customers')->delete();
-        
-        \DB::table('users_customers')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
+        \DB::statement("ALTER SEQUENCE users_customers_id_seq RESTART WITH 1;");
+        \DB::table('users_customers')->insert(array(
+            0 =>
+            array(
                 'user_id' => 1,
                 'customer_id' => 1,
                 'created_at' => '2025-07-16 14:14:02',
                 'updated_at' => '2025-07-16 14:14:02',
                 'deleted_at' => NULL,
             ),
-            1 => 
-            array (
-                'id' => 2,
+            1 =>
+            array(
                 'user_id' => 1,
                 'customer_id' => 2,
                 'created_at' => '2025-07-16 14:14:24',
                 'updated_at' => '2025-07-16 14:14:24',
                 'deleted_at' => NULL,
             ),
-            2 => 
-            array (
-                'id' => 3,
+            2 =>
+            array(
                 'user_id' => 1,
                 'customer_id' => 3,
                 'created_at' => '2025-07-16 14:14:41',
@@ -47,7 +44,5 @@ class UsersCustomersTableSeeder extends Seeder
                 'deleted_at' => NULL,
             ),
         ));
-        
-        
     }
 }
