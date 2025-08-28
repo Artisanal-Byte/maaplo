0c4059660d40572497cdc2a55d2cc411890ff2ef
Goswami Sahil <goswamisahil1911@gmail.com>
2025-08-28 16:46:33 +0530

    add gst logic

--- resources/js/pages/organization/Create.vue
+++ resources/js/pages/organization/Create.vue
@@ -120,7 +120,9 @@ const createOrganization = () => {
             <Loader v-if="loading" :message="'Creating Your Organization...'" />
             <!-- Form -->
             <div class="lg:mt-10 gap-3 bg-white lg:p-7 rounded-lg shadow-md p-5 border-t-4 border-primary">
-                <h1 class="text-2xl text-primary font-bold text-gray-800 font-[Convergence] flex items-center gap-2 mb-3">Organization Details</h1>
+                <h1
+                    class="text-2xl text-primary font-bold text-gray-800 font-[Convergence] flex items-center gap-2 mb-3">
+                    Organization Details</h1>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <!-- Organization Name -->
                     <Input v-model="form.organization_name" label="Organization Name"
@@ -133,6 +135,9 @@ const createOrganization = () => {
                     <!-- GST Number -->
                     <Input v-model="form.gst_number" label="GST Number" placeholder="Enter GST Number"
                         :error="form.errors.gst_number" @input="form.gst_number = form.gst_number.toUpperCase()">
+                    <template #hint>
+                        <span class="text-gray-400 text-sm"> (If applicable)</span>
+                    </template>
                     <template #icon>
                         <Icon icon="mdi:certificate-outline" width="24" height="24" />
                     </template>
@@ -186,7 +191,10 @@ const createOrganization = () => {
                     </div>
                 </div>
                 <!-- Bank Account Details -->
-                 <h1 class="text-2xl text-primary font-bold text-gray-800 font-[Convergence] flex items-center gap-2 my-5">Account Details</h1>
+                <h1
+                    class="text-2xl text-primary font-bold text-gray-800 font-[Convergence] flex items-center gap-2 my-5">
+                    Account
+                    Details</h1>
                 <div class=" grid grid-cols-1 md:grid-cols-2 gap-6">
                     <Input v-model="form.account_holder_name" label="Account Holder Name"
                         placeholder="Enter Account Holder Name" :error="form.errors.account_holder_name"
@@ -282,7 +290,7 @@ const createOrganization = () => {
                     <div class="mb-4">
                         <div
                             class="w-full h-64 bg-gray-100 border border-primary rounded flex items-center justify-center overflow-hidden">
-                            <img v-if="qrUrl" :src="qrUrl"  alt="QR Payment" class="object-contain h-full w-full" />
+                            <img v-if="qrUrl" :src="qrUrl" alt="QR Payment" class="object-contain h-full w-full" />
                         </div>
                     </div>
                     <input type="file" @change="handleQRChange"
