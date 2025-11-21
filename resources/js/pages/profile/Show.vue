9736bfd9e951b32b1b44eac99d3b69fcbb1d2f00
sahilgoswami5180 <sgoswamisahil@gmail.com>
2025-11-21 15:38:44 +0530

    test for deploy 2th time

--- resources/js/pages/profile/Show.vue
+++ resources/js/pages/profile/Show.vue
@@ -1,40 +1,34 @@
 <script setup>
-import AppLayout from '@/layouts/AppLayout.vue';
-import { Head, Link, usePage } from '@inertiajs/vue3';
-import { router } from '@inertiajs/vue3';
-import { Icon } from '@iconify/vue';
 import Button from '@/components/Button.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Icon } from '@iconify/vue';
+import { Head, Link, router } from '@inertiajs/vue3';
 const toast = new ToastMagic();
 const props = defineProps(['user']);
 function goToEdit() {
     router.visit(route('profile.edit'));
 }
-
 </script>
 
 <template>
     <Head :title="`${props.user.name}-Profile`" />
     <AppLayout>
-        <div class="px-4 lg:px-0 py-10 max-w-4xl mx-auto">
-            <div class="flex justify-between items-center mb-6">
+        <div class="mx-auto max-w-4xl px-4 py-10 lg:px-0">
+            <div class="mb-6 flex items-center justify-between">
                 <div class="flex">
-                    <Icon icon="healthicons:ui-user-profile" width="42" height="42"
-                        class="text-xl font-bold text-primary" />
-                    <h1 class="text-3xl font-bold text-primary mt-1 ml-3">
-                        Profile
-                    </h1>
+                    <Icon icon="healthicons:ui-user-profile" width="42" height="42" class="text-xl font-bold text-primary" />
+                    <h1 class="ml-3 mt-1 text-3xl font-bold text-primary">Profile</h1>
                 </div>
                 <Link :href="route('dashboard')" class="flex items-center gap-2 text-gray-500 hover:text-gray-800">
-                <Icon icon="material-symbols:arrow-back-rounded" width="24" height="24" />
-                <span class="text-base font-medium">Back</span>
+                    <Icon icon="material-symbols:arrow-back-rounded" width="24" height="24" />
+                    <span class="text-base font-medium">Back</span>
                 </Link>
             </div>
 
-            <div
-                class="bg-[#DEEFF4] shadow-lg rounded-xl p-3 lg:p-6 transition-all hover:shadow-xl border border-gray-100">
+            <div class="rounded-xl border border-gray-100 bg-[#DEEFF4] p-3 shadow-lg transition-all hover:shadow-xl lg:p-6">
                 <div class="grid grid-cols-3">
-                    <div class="space-y-4 grid grid-cols-1 text-lg col-span-2">
-                        <div class="flex items-center gap-3 mt-3">
+                    <div class="col-span-2 grid grid-cols-1 space-y-4 text-lg">
+                        <div class="mt-3 flex items-center gap-3">
                             <Icon icon="mdi:account" class="text-gray-500" width="20" />
                             <p><strong>Name:</strong> {{ props.user.name }}</p>
                         </div>
@@ -49,11 +43,6 @@ function goToEdit() {
                             <p><strong>Phone:</strong> {{ props.user.phone }}</p>
                         </div>
 
-                        <!-- <div class="flex items-center gap-3">
-                            <Icon icon="mdi:office-building" class="text-gray-500" width="20" />
-                            <p><strong>Organization:</strong> {{ props.user.organization_name ?? 'N/A' }}</p>
-                        </div> -->
-
                         <div class="flex items-center gap-3">
                             <Icon icon="mdi:badge-account-outline" class="text-gray-500" width="20" />
                             <p><strong>Subscription:</strong> {{ props.user.subscription_plan }}</p>
@@ -61,7 +50,8 @@ function goToEdit() {
 
                         <div class="flex items-center gap-3">
                             <Icon icon="mdi:calendar-check" class="text-gray-500" width="20" />
-                            <p><strong>Validity:</strong>
+                            <p>
+                                <strong>Validity:</strong>
                                 {{ props.user.formatted_validity ?? 'N/A' }}
                             </p>
                         </div>
@@ -75,15 +65,16 @@ function goToEdit() {
 
                     <div class="w-full text-right">
                         <!-- <Link :href="route('profile.show')"> -->
-                        <img :src="props.user.avatar ? `/storage/${props.user.avatar.replace(/^storage\//, '')}` : '/images/man_avatar.avif'"
+                        <img
+                            :src="props.user.avatar ? `/storage/${props.user.avatar.replace(/^storage\//, '')}` : '/images/man_avatar.avif'"
                             alt="Profile Image"
-                            class="lg:w-48 lg:h-48 w-24 h-24 mr-0 lg:mr-12 mt-3 lg:mt-10 inline-block rounded-full object-cover border" />
+                            class="mr-0 mt-3 inline-block h-24 w-24 rounded-full border object-cover lg:mr-12 lg:mt-10 lg:h-48 lg:w-48"
+                        />
                         <!-- </Link> -->
                     </div>
                 </div>
                 <!-- <div class="pt-6 text-center"> -->
-                <Button @click="goToEdit"
-                    class="mt-6 w-full w-auto bg-primary text-white hover:bg-primary-dark transition-colors">
+                <Button @click="goToEdit" class="hover:bg-primary-dark mt-6 w-auto w-full bg-primary text-white transition-colors">
                     Edit Profile
                 </Button>
                 <!-- </div> -->
